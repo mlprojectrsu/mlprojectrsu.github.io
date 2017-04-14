0456e6e08ae59a422a678b6f41c297d599763cc7
Rashmi Raghunandan <Rashmi Raghunandan>
2017-04-13 20:55:36 -0400

    php test

---
+++ welcome_get.php
@@ -0,0 +1,4 @@
+I was very happy to get this opportunity.I no longer will be able to join you.
+Why?
+It does not completely allign with my career goals. I recieved an offer from another dream company. I wanted to infomr you as eary as possible so
+as to not inconvinience you any further.
