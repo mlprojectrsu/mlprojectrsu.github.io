0335b1615da49ca322f96aa3847542cc3134ac26
Rashmi Raghunandan <Rashmi Raghunandan>
2017-04-13 20:58:28 -0400

    php test

--- welcome_get.php
+++ welcome_get.php
@@ -1,4 +1,8 @@
-I was very happy to get this opportunity.I no longer will be able to join you.
-Why?
-It does not completely allign with my career goals. I recieved an offer from another dream company. I wanted to infomr you as eary as possible so
-as to not inconvinience you any further.
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+
+</body>
+</html>
